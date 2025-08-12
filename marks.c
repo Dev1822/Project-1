9b3090817d04b96dbe635d4a9a48d416c93310df
Dev1822 <dev.d.patel.cg@gmail.com>
2025-08-12 15:59:13 +0530

    Grades

---
+++ marks.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int marks;
+    printf("Enter Marks :");
+    scanf("%d",&marks);
+    if(marks>=90){
+        printf("Excellent");
+    }
+    else if(marks>=75){
+        printf("Good");
+    }
+    else if(marks>=50){
+        printf("Average");
+    }
+    else{
+        printf("Fail");
+    }
+    return 0;
+}
